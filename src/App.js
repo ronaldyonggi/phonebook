b8d5a0ccc19edf395df23a1eb8bfbdce7e858bd9
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-11 12:28:58 -0400

    2.6 completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
 
 function App() {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas'}
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleInputChange = event => {
+    setNewName(event.target.value)
+  }
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault()
+    const newPersons = persons.concat({
+      name: newName
+    })
+    setPersons(newPersons)
+  }
+
   return (
-    <div className="App">
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmitForm}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <li key={person.name}>{person.name}</li>)}
     </div>
   );
 }
