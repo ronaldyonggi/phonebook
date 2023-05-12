b384bc644846bb08249b7582f72527fdc749aeaf
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-12 10:46:22 -0400

    2.11 completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import DisplayPerson from "./components/DisplayPersons";
+import axios from "axios";
 
 const App = () => {
-  const initialPersons = [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]
   
-  const [persons, setPersons] = useState(initialPersons)
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    async function fetchPersons() {
+      const response = await axios.get('http://localhost:3001/persons')
+      const persons = await response.data
+      setPersons(persons)
+    }
+
+    fetchPersons()
+  },[])
 
   const handleNameInputChange = event => {
     setNewName(event.target.value)
